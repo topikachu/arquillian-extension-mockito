0bb432883c359a7024a5a2310e7ba301350281c1
topikachu <topikachu@163.com>
2014-03-07 09:44:59 +0800

    A working project with impl and test

--- src/test/java/org/jboss/arquillian/extension/mockito/MockitoIntegrationTest.java
+++ src/test/java/org/jboss/arquillian/extension/mockito/MockitoIntegrationTest.java
@@ -7,41 +7,54 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.Resolvers;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenResolverSystem;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Spy;
+
 import static org.mockito.Mockito.*;
 
 @RunWith(Arquillian.class)
 public class MockitoIntegrationTest {
 	@Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-            .addClass(BarResource.class)
-            .addClass(FooService.class)
-            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
+	public static WebArchive createDeployment() {
+
+		return ShrinkWrap
+				.create(WebArchive.class, "ROOT.war")
+				.addClass(BarResource.class)
+				.addClass(FooService.class)
+				.addAsLibraries(
+						Resolvers
+								.use(MavenResolverSystem.class)
+								.loadPomFromFile("pom.xml", "tomcat-embedded-7")
+								.resolve("org.jboss.weld.servlet:weld-servlet")
+								.withTransitivity().asFile())
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+				.setWebXML("test-web.xml");
+	}
+
 	@Inject
 	private FooService fooService;
-	
-	@Inject @Spy
+
+	@Inject
+	@Spy
 	private BarResource barResource;
-	
-	
+
 	@Before
-	public void beforeTest(){
-		
+	public void beforeTest() {
+
 	}
-	
+
 	@Test
 	public void testFoo() {
 		reset(barResource);
-	when(barResource.getText()).thenReturn("hello world");
+		when(barResource.getText()).thenReturn("hello world");
 		fooService.execute();
-		
 		verify(barResource).getText();
-		
+
 	}
 
 }
